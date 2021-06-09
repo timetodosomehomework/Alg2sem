a81f2a90d3fe8d893664cee5ab16015f6a25606f
timetodosomehomework <58882714+timetodosomehomework@users.noreply.github.com>
2021-06-09 11:48:21 +0300

    Add files via upload

---
+++ kurs/kurstest.cpp
@@ -0,0 +1,36 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\kurs\flow.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace flow;
+namespace kurstest
+{
+	TEST_CLASS(kurstest)
+	{
+	public:
+		
+		TEST_METHOD(readtest)
+		{
+			ifstream file("F:\\test.txt");
+			string line; int size;
+			getline(file, line);
+			size = atoi(line.c_str());
+			char* nodes = new char[size];
+			int** Matrix = new int* [size];
+			for (int i = 0; i < size; i++)
+				Matrix[i] = new int[size];
+			read(file, Matrix, size, nodes);
+			char nodestest[3] = { 'A', 'S', 'C' };
+			int Matrixtest[3][3] = { {0,12,0},{0,0,4},{0,0,0} };
+			for (int i = 0; i < size; i++)
+			{
+				Assert::IsTrue(nodes[i] == nodestest[i]);
+				for (int j = 0; j < size; j++)
+				{
+					Assert::AreEqual(Matrix[i][j], Matrixtest[i][j]);
+				}
+			}
+		}
+	};
+}
